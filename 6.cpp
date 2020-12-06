e799a3b107f157fde2878e660fb632e1013b512b
Massimiliano Curzi <massimiliano.curzi@gmail.com>
2020-12-06 06:43:30 +0000

    day 6 c++

--- 6.cpp
+++ 6.cpp
@@ -3,17 +3,43 @@
 using namespace std;
 
 const string inputname = "6.in";
-using input_list_t = set<int>;
-using solution_t = int;
+using pass_answ_t = char;
+using group_answ_t = set<pass_answ_t>;
+using input_list_1_t = vector<group_answ_t>;
+using input_list_2_t = vector<input_list_1_t>;
+using sol_t = int;
 
-solution_t one(input_list_t & sids)
+sol_t one(const input_list_1_t & ls)
 {
-    return -1;
+    sol_t tot = 0;
+    for (auto & l: ls)
+    {
+        tot+= l.size();
+    }
+    return tot;
 }
 
-solution_t two(input_list_t & sids)
+sol_t two(const input_list_2_t & ls)
 {
-    return -1;
+    sol_t tot = 0;
+    for (auto & ll: ls)
+    {
+        if (ll.empty()) continue;
+        group_answ_t sint = ll[0];
+        for (auto & s: ll)
+        {
+            if (!s.empty() && !sint.empty())
+            {
+                group_answ_t sint_temp{};
+                std::set_intersection(sint.begin(), sint.end(),
+                                      s.begin(), s.end(),
+                                      std::inserter(sint_temp, sint_temp.begin()));
+                sint = sint_temp;
+            }
+        }
+        tot += sint.size();
+    }
+    return tot;
 }
 
 set<int> sids;
@@ -21,13 +47,51 @@ int main(int argc, char **kwargs)
 {
     ifstream infile(inputname);
     string s;
-    while( getline( infile, s ) )
+    group_answ_t q{};
+    input_list_1_t l1;
+    input_list_1_t l2;
+    input_list_2_t l3;
+    
+    while( getline( infile, s ) ) // for each line read from the file
     {
-        //
+        for (char & c: s)
+        {
+            if (c)
+            {
+                q.insert(c);
+            }
+        }
+        if (s=="")
+        {
+            l1.push_back(q);
+            q = {};
+        }
     }
+    l1.push_back(q);
+    q = {};
+    infile.clear();
+    infile.seekg(0);
+    while( getline( infile, s ) ) // for each line read from the file
+    {
+        for (char & c: s)
+        {
+            if (c)
+            {
+                q.insert(c);
+            }
+        }
+        l2.push_back(q);
+        q = {};
+        if (s=="")
+        {
+            l3.push_back(l2);
+            l2 = {};
+        }
+    }
+    l3.push_back(l2);
          
-    std::cout << "one = " << one(sids) << '\n'; // 980
-    std::cout << "two = " << two(sids) << '\n'; // 607
+    std::cout << "one = " << one(l1) << '\n'; // 6249
+    std::cout << "two = " << two(l3) << '\n'; // 3103
 
     return 0;
 }
